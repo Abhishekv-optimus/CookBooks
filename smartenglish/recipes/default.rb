087809727167a7ea5c179e23cc5eb085e414ea56
abhishekv-optimus <abhishek.v@optimusinfo.com>
2014-03-04 12:58:11 +0530

    Added changes

--- smartenglish/recipes/default.rb
+++ smartenglish/recipes/default.rb
@@ -20,23 +20,23 @@ execute "download mysqldump" do
   command "sh /opt/setup/mysql_dump.sh"
 end
 
-cookbook_file "/opt/setup/setup-system.sh" do
-  source "setup-system.sh"
-  mode 0755
-end
+#cookbook_file "/opt/setup/setup-system.sh" do
+#  source "setup-system.sh"
+#  mode 0755
+#end
 
-execute "install setup-system.sh" do
-  command "sh /opt/setup/setup-system.sh"
-end
+#execute "install setup-system.sh" do
+#  command "sh /opt/setup/setup-system.sh"
+#end
 
-cookbook_file "/opt/setup/setup-cloudwatch.sh" do
-  source "setup-cloudwatch.sh"
-  mode 0755
-end
+#cookbook_file "/opt/setup/setup-cloudwatch.sh" do
+#  source "setup-cloudwatch.sh"
+#  mode 0755
+#end
 
-execute "install setup-cloudwatch.sh" do
-  command "sh /opt/setup/setup-cloudwatch.sh"
-end
+#execute "install setup-cloudwatch.sh" do
+#  command "sh /opt/setup/setup-cloudwatch.sh"
+#end
 
 #cookbook_file "#{node["smrt_document_root"]}/include/constants.php"
 # source "constants.php"
