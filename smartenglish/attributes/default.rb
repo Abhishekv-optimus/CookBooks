0d03fa3629d5b3f8aa069bfe2d8bbf2297085966
abhishekv-optimus <abhishek.v@optimusinfo.com>
2014-03-04 17:50:18 +0530

    Added host in smartenglish

--- smartenglish/attributes/default.rb
+++ smartenglish/attributes/default.rb
@@ -1,5 +1,5 @@
 default["smrt_document_root"] = "/srv/www/smrt/current"
-default["smrt_db_host_name"] = "sample_db_host"
+default["smrt_db_host_name"] = "sdvlxlalni8zj.clie6s5lawsl.us-east-1.rds.amazonaws.com"
 default["smrt_db_username"] = "smrt_db_usr"
 default["smrt_db_password"] = "8T01c52909"
 default["smrt_db_name"] = "smrt_production"
